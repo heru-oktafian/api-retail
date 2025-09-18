6324bea7e7c23be50c74aa5b6481a31f566f9000
Heru Oktafian, ST., CTT <heru@heruoktafian.com>
2025-09-19 06:21:28 +0700

    feat(controllers): implement CRUD operations for unit management

---
+++ controllers/master_unit_controller.go
@@ -0,0 +1,117 @@
+package controllers
+
+import (
+	"math"
+	"strings"
+
+	models "github.com/heru-oktafian/api-retail/models"
+	config "github.com/heru-oktafian/scafold/config"
+	framework "github.com/heru-oktafian/scafold/framework"
+	helpers "github.com/heru-oktafian/scafold/helpers"
+	middlewares "github.com/heru-oktafian/scafold/middlewares"
+	responses "github.com/heru-oktafian/scafold/responses"
+)
+
+// CreateUnit buat unit
+func CreateUnit(c *framework.Ctx) error {
+	branch_id, _ := middlewares.GetBranchID(c)
+	return helpers.CreateResource(c, config.DB, &models.Unit{}, branch_id, "UNT")
+}
+
+// UpdateUnit update unit
+func UpdateUnit(c *framework.Ctx) error {
+	id := c.Params("id")
+	// Updating unit using helpers
+	return helpers.UpdateResource(c, config.DB, &models.Unit{}, id)
+}
+
+// DeleteUnit hapus unit
+func DeleteUnit(c *framework.Ctx) error {
+	id := c.Params("id")
+	// Deleting unit using helpers
+	return helpers.DeleteResource(c, config.DB, &models.Unit{}, id)
+}
+
+// GetUnitByID tampilkan unit berdasarkan id
+func GetUnitByID(c *framework.Ctx) error {
+	id := c.Params("id")
+	// Getting unit using helpers
+	return helpers.GetResource(c, config.DB, &models.Unit{}, id)
+}
+
+// GetAllUnit tampilkan semua unit
+func GetAllUnit(c *framework.Ctx) error {
+	// Ambil ID cabang
+	branch_id, _ := middlewares.GetBranchID(c)
+
+	// Parsing body JSON ke struct
+	var body models.RequestBody
+	if err := c.BodyParser(&body); err != nil {
+		return responses.JSONResponse(c, framework.StatusBadRequest, "Body permintaan tidak valid", "Gagal mem-parsing body permintaan")
+	}
+
+	// Validasi dan set default untuk halaman jika tidak valid
+	page := body.Page
+	if page < 1 {
+		page = 1
+	}
+	limit := 10                              // Tetapkan batas data per halaman ke 10
+	search := strings.TrimSpace(body.Search) // Ambil kata kunci pencarian dari body
+	offset := (page - 1) * limit
+
+	var Unit []models.AllUnit // Gunakan AllUnit untuk mengambil data unit tanpa branch_id
+	var total int64
+
+	// Query dasar
+	query := config.DB.Table("units un").Select("un.id AS unit_id, un.name AS unit_name").Where("un.branch_id = ?", branch_id)
+
+	// Jika ada kata kunci pencarian, tambahkan filter WHERE
+	if search != "" {
+		search = strings.ToLower(search) // Konversi kata kunci pencarian ke huruf kecil
+		query = query.Where("LOWER(un.name) LIKE ?", "%"+search+"%")
+	}
+
+	// Hitung total unit yang sesuai dengan filter
+	if err := query.Count(&total).Error; err != nil {
+		return responses.JSONResponse(c, framework.StatusInternalServerError, "Gagal mengambil data Unit", "Gagal menghitung jumlah Unit")
+	}
+
+	// Ambil data dengan pagination
+	if err := query.Offset(offset).Limit(limit).Scan(&Unit).Error; err != nil {
+		return responses.JSONResponse(c, framework.StatusInternalServerError, "Gagal mengambil data Unit", "Gagal mengambil data Unit dari database")
+	}
+
+	// Hitung total halaman berdasarkan hasil filter
+	totalPages := int(math.Ceil(float64(total) / float64(limit)))
+
+	return responses.JSONResponseGetAll(c, framework.StatusOK, "Data Unit berhasil diambil", search, int(total), page, int(totalPages), int(limit), Unit)
+}
+
+// CmbUnit mendapatkan semua kategori unit
+func CmbUnit(c *framework.Ctx) error {
+	// Get branch id
+	branch_id, _ := middlewares.GetBranchID(c)
+
+	// Parse query parameters for search
+	search := strings.TrimSpace(c.Query("search", ""))
+
+	var cmbUnits []models.UnitCombo
+
+	// Base query to get all unit categories
+	query := config.DB.Table("units").
+		Select("id as unit_id, name as unit_name").
+		Where("branch_id = ?", branch_id)
+
+	// If search parameter is provided, add a filter
+	if search != "" {
+		search = strings.ToLower(search) // Convert search keyword to lowercase
+		query = query.Where("LOWER(name) LIKE ?", "%"+search+"%")
+	}
+
+	// Execute the query
+	if err := query.Find(&cmbUnits).Error; err != nil {
+		return responses.JSONResponse(c, framework.StatusInternalServerError, "Failed to get data", "Failed to get data")
+	}
+
+	return responses.JSONResponse(c, framework.StatusOK, "Data berhasil ditemukan", cmbUnits)
+}
--- go.mod
+++ go.mod
@@ -2,14 +2,21 @@ module github.com/heru-oktafian/api-retail
 
 go 1.25.1
 
-require github.com/heru-oktafian/scafold v1.0.2
+require (
+	github.com/heru-oktafian/scafold v1.0.4
+	golang.org/x/crypto v0.41.0
+	gorm.io/gorm v1.25.10
+)
 
 require (
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/gofiber/contrib/jwt v1.1.2 // indirect
 	github.com/gofiber/fiber/v2 v2.52.9 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -25,10 +32,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
 	gorm.io/driver/postgres v1.6.0 // indirect
-	gorm.io/gorm v1.25.10 // indirect
 )
