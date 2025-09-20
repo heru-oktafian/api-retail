ddaa6fc03960c785805a714d7a48749bcf9e8b66
Heru Oktafian, ST., CTT <heru@heruoktafian.com>
2025-09-20 13:12:07 +0700

    Update

--- routes/test_route.go
+++ routes/test_route.go
@@ -1,13 +1,18 @@
 package routes
 
-import "github.com/heru-oktafian/scafold/framework"
+import (
+	"os"
+
+	"github.com/heru-oktafian/scafold/framework"
+)
 
 func CobaRoutes(app *framework.Fiber) {
+	JWTSecret := os.Getenv("JWT_SECRET_KEY")
 	cobaAPI := app.Group("/coba")
 	cobaAPI.Get("/", func(c *framework.Ctx) error {
 		return c.JSON(200, map[string]interface{}{
 			"status":  200,
-			"message": "Coba endpoint hit",
+			"message": "Coba endpoint hit successfully : " + JWTSecret,
 			"data":    nil,
 		})
 	})
