8cd21b2f23f626da88611e7504ad63f6cf814516
Heru Oktafian, ST., CTT <heru@heruoktafian.com>
2025-09-20 07:19:22 +0700

    Update

--- routes/master_unit_routes.go
+++ routes/master_unit_routes.go
@@ -6,33 +6,60 @@ import (
 	"github.com/heru-oktafian/api-retail/controllers"
 	"github.com/heru-oktafian/scafold/framework"
 	"github.com/heru-oktafian/scafold/middlewares"
-	"github.com/heru-oktafian/scafold/responses"
+	// "github.com/heru-oktafian/scafold/responses"
 )
 
 // MasterUnitRoutes mengatur rute-rute untuk resource unit
+// func MasterUnitRoutes(app *framework.Fiber) {
+// 	// Load Secret Key from environment
+// 	JWTSecret := os.Getenv("JWT_SECRET_KEY")
+
+// 	// Group routes that require authentication
+// 	unitAPI := app.Group("/api/units")
+
+// 	unitAPI.Get("/", controllers.GetAllUnit, middlewares.Protected(JWTSecret),
+// 		middlewares.AuthorizeRole("operator", "cashier", "finance", "superadmin", "administrator"))
+
+// 	unitAPI.Get("/:id", controllers.GetUnitByID, middlewares.Protected(JWTSecret),
+// 		middlewares.AuthorizeRole("operator", "cashier", "finance", "superadmin", "administrator"))
+
+// 	unitAPI.Post("/", controllers.CreateUnit,
+// 		middlewares.AuthorizeRole("operator", "superadmin", "administrator"))
+
+// 	unitAPI.Put("/:id", controllers.UpdateUnit,
+// 		middlewares.AuthorizeRole("operator", "superadmin", "administrator"))
+
+// 	unitAPI.Delete("/:id", controllers.DeleteUnit,
+// 		middlewares.AuthorizeRole("superadmin", "administrator"))
+
+// 	unitAPI.Get("/coba", func(c *framework.Ctx) error {
+// 		return responses.JSONResponse(c, 200, "Coba endpoint hit", nil)
+// 	})
+// }
+
 func MasterUnitRoutes(app *framework.Fiber) {
 	// Load Secret Key from environment
 	JWTSecret := os.Getenv("JWT_SECRET_KEY")
 
-	// Group routes that require authentication
+	// Grup rute yang dilindungi JWT
 	unitAPI := app.Group("/api/units")
 
-	unitAPI.Get("/", controllers.GetAllUnit, middlewares.Protected(JWTSecret),
-		middlewares.AuthorizeRole("operator", "cashier", "finance", "superadmin", "administrator"))
+	// GET /api/units -> ambil semua unit
+	unitAPI.Get("/", controllers.GetAllUnit)
 
-	unitAPI.Get("/:id", controllers.GetUnitByID, middlewares.Protected(JWTSecret),
-		middlewares.AuthorizeRole("operator", "cashier", "finance", "superadmin", "administrator"))
+	// GET /api/units/:id -> ambil unit berdasarkan ID
+	unitAPI.Get("/:id", controllers.GetUnitByID)
 
-	unitAPI.Post("/", controllers.CreateUnit,
-		middlewares.AuthorizeRole("operator", "superadmin", "administrator"))
+	// POST /api/units -> buat unit baru
+	unitAPI.Post("/", controllers.CreateUnit, middlewares.Protected(JWTSecret))
 
-	unitAPI.Put("/:id", controllers.UpdateUnit,
-		middlewares.AuthorizeRole("operator", "superadmin", "administrator"))
-
-	unitAPI.Delete("/:id", controllers.DeleteUnit,
-		middlewares.AuthorizeRole("superadmin", "administrator"))
-
+	// GET /api/units/:id -> ambil unit berdasarkan ID
 	unitAPI.Get("/coba", func(c *framework.Ctx) error {
-		return responses.JSONResponse(c, 200, "Coba endpoint hit", nil)
+		return c.JSON(200, map[string]interface{}{
+			"status":  200,
+			"message": "Coba endpoint hit",
+			"data":    nil,
+		})
 	})
+
 }
