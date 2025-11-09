f47010c22889619ab742414bfde3c2273ae09ba4
Heru Oktafian <heru@heruoktafian.com>
2025-11-09 22:39:53 +0700

    Fixing scheduler function

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -15,10 +15,10 @@ func InitScheduler(db *gorm.DB) *cron.Cron {
 	// === CONTOH JOB ===
 
 	// 1. Backup DB setiap pukul 02:00 WIB (UTC+7 → 19:00 UTC kemarin)
-	c.AddFunc("0 19 * * *", func() {
-		log.Println("[SCHEDULER] Mulai backup database...")
-		BackupDatabase()
-	})
+	// c.AddFunc("0 19 * * *", func() {
+	// 	log.Println("[SCHEDULER] Mulai backup database...")
+	// 	BackupDatabase()
+	// })
 
 	// 2. Hitung asset value & simpan dalam tabel sys_dayly_asset setiap pukul 21:35 WIB (UTC-7 → 15:35 UTC kemarin)
 	c.AddFunc("35 15 * * *", func() {
@@ -31,22 +31,22 @@ func InitScheduler(db *gorm.DB) *cron.Cron {
 	})
 
 	// 3. Generate laporan harian pukul 06:00 WIB (23:00 UTC)
-	c.AddFunc("0 23 * * *", func() {
-		log.Println("[SCHEDULER] Generate laporan harian...")
-		GenerateDailyReport()
-	})
+	// c.AddFunc("0 23 * * *", func() {
+	// 	log.Println("[SCHEDULER] Generate laporan harian...")
+	// 	GenerateDailyReport()
+	// })
 
 	// 4. Reset cache Redis setiap pukul 00:00 WIB
-	c.AddFunc("0 17 * * *", func() { // 17:00 UTC = 00:00 WIB
-		log.Println("[SCHEDULER] Clearing Redis cache...")
-		ClearRedisCache()
-	})
+	// c.AddFunc("0 17 * * *", func() { // 17:00 UTC = 00:00 WIB
+	// 	log.Println("[SCHEDULER] Clearing Redis cache...")
+	// 	ClearRedisCache()
+	// })
 
 	// 5. Cek expired promo tiap 10 menit
-	c.AddFunc("*/10 * * * *", func() {
-		log.Println("[SCHEDULER] Cek promo kadaluarsa...")
-		DeactivateExpiredPromos()
-	})
+	// c.AddFunc("*/10 * * * *", func() {
+	// 	log.Println("[SCHEDULER] Cek promo kadaluarsa...")
+	// 	DeactivateExpiredPromos()
+	// })
 
 	c.Start()
 	log.Println("[SCHEDULER] Semua job terjadwal aktif!")
